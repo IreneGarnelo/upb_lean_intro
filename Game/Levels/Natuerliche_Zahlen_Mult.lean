5873ff8202a1e665afaf92469db9b463597c8cd2
ireneg <irene.garnelo.abellanas@uni-paderborn.de>
2026-03-08 12:45:01 +0100

    [Level Import]: Fixed bug case-sesitive

--- Game/Levels/Natuerliche_Zahlen_Mult.lean
+++ Game/Levels/Natuerliche_Zahlen_Mult.lean
@@ -1,7 +1,7 @@
 import Game.Levels.Natuerliche_Zahlen_Mult.L01_Multiplikation
 import Game.Levels.Natuerliche_Zahlen_Mult.L02_succ_mul
 import Game.Levels.Natuerliche_Zahlen_Mult.L03_mul_comm
-import Game.Levels.Natuerliche_Zahlen_Mult.L04_Left_distrib
+import Game.Levels.Natuerliche_Zahlen_Mult.L04_left_distrib
 import Game.Levels.Natuerliche_Zahlen_Mult.L05_mul_assoc
 import Game.Levels.Natuerliche_Zahlen_Mult.L06_mul_gerade
 
